4e3301454d2a12f90450b89087339467fd4f2c60
xinyihl <1012737146@qq.com>
2023-07-09 10:57:48 +0800

    fix

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,6 @@
 async function onLoad() {
     const settings = await remove_sidebar.getSettings();
-    const arr = settings.remove。split(',');
+    const arr = settings.remove.split(',');
     const interval = setInterval(() => {
     if (LiteLoader.os.platform == "win32") {
         var buttons = document.querySelectorAll(".sidebar-nav .nav-item");
@@ -12,7 +12,7 @@ async function onLoad() {
         for(let item of arr) {
             console.log(item);
             const lastButton = buttons[item-1];
-            lastButton.parentNode。removeChild(lastButton);
+            lastButton.parentNode.removeChild(lastButton);
         }
         clearInterval(interval);
     }
@@ -20,19 +20,19 @@ async function onLoad() {
 }
 
 async function onConfigView(view) {
-    const plugin_path = betterQQNT.plugins。remove_sidebar。path。plugin;
+    const plugin_path = betterQQNT.plugins.remove_sidebar.path.plugin;
     const css_file_path = `file:///${plugin_path}/src/settings.css`;
     const html_file_path = `file:///${plugin_path}/src/settings.html`;
 
-    const css_text = await (await fetch(css_file_path))。text();
+    const css_text = await (await fetch(css_file_path)).text();
     const style = document.createElement("style");
     style.textContent = css_text;
     view.appendChild(style);
 
-    const html_text = await (await fetch(html_file_path))。text();
+    const html_text = await (await fetch(html_file_path)).text();
     const parser = new DOMParser();
     const doc = parser.parseFromString(html_text, "text/html");
-    doc.querySelectorAll("section")。forEach(node => view.appendChild(node));
+    doc.querySelectorAll("section").forEach(node => view.appendChild(node));
 
     const settings = await remove_sidebar.getSettings();
 
